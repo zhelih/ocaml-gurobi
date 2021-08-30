3254251a0b32ba77b681f4265f6e9658541a268e
Eugene Lykhovyd <lykhovyd@gmail.com>
2021-08-30 04:25:17 +0000

    sample test case for bindings

--- example/example.ml
+++ example/example.ml
@@ -1,11 +1,13 @@
 open Gurobi
+open Ctypes
 
-let () = ()
-(*
 let () =
-  try
-    let env = C.loadenv () in
-    let model = C.readmodel env "test.lp" in
-    C.optimize model
-  with C.GRBError (_,code,s) -> Printf.printf "Failed with exception %s (%d)\n" s code
-*)
+  let env_ptr = allocate (ptr grb_Env) (from_voidp grb_Env null) in
+  let err1 = loadenv env_ptr "gurobi.log" in
+  let env = !@ env_ptr in
+
+  let model_ptr = allocate (ptr grb_Model) (from_voidp grb_Model null) in
+  let err2 = readmodel env "test.lp" model_ptr in
+  let model = !@ model_ptr in
+  let err3 = optimize model in
+  ()
--- lib/gurobi.ml
+++ lib/gurobi.ml
@@ -1,4 +1,11 @@
 open Ctypes
 open Foreign
 
-module Stubs = Bindings.Enums(Bindings_stubs)
+type grb_Env
+let grb_Env : grb_Env structure typ = structure "GRBEnv"
+type grb_Model
+let grb_Model : grb_Model structure typ = structure "GRBModel"
+
+let optimize = foreign "GRBoptimize" ((ptr grb_Model) @-> returning int)
+let loadenv = foreign "GRBloadenv" ((ptr (ptr grb_Env))  @-> string @-> returning int)
+let readmodel = foreign "GRBreadmodel" ((ptr grb_Env) @-> string @-> (ptr (ptr grb_Model)) @-> returning int)
